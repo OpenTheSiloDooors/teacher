fc4d140014767371c172edf03a6260b6cbce028a
OpenTheSiloDooors <jacob.espinoza@malad.us>
2025-04-07 22:55:11 -0700

    Fix bugs

--- errors/HelloWorldBad.java
+++ errors/HelloWorldBad.java
@@ -1,12 +1,17 @@
+/*
++ =
+
+~ moved }
+ */
 
 class HelloWorldBad {
     public static void main(String[] args) {
-        
-        for (int i=0;i<25;i++){
-            System.out.println("the value of i is "+i);
-            if (i=1){
+
+        for (int i = 0; i < 25; i++) {
+            System.out.println("the value of i is " + i);
+            if (i == 1) {
                 System.out.println("i is equal to 1");
             }
-        }    
         }
     }
+}
--- errors/error.java
+++ errors/error.java
@@ -3,23 +3,44 @@ Error filled file created by
 Kimg@techtrepacademy.com
 for my java class to learn troubleshooting on
 */
-public class Main {
-    System.out.println("hello everybody");
-    System.out.println("am I doing this correctly");
-    for (int i=0;i<10;i++){
-        System.out.printlm("the number i is "+i);
+
+/*
++ method
++ intialize int
++ )
++ two }
+
+~ printlm > println
+~ For > for
+~ Int > int
+~ system > System
+~ : > ;
+~ i > j
+~ print > println
+
+- removed two }
+ */
+
+public class error {
+    public static void main(String[] args) {
+
+        System.out.println("hello everybody");
+        System.out.println("am I doing this correctly");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("the number i is " + i);
+
+            for (int j = 0; i > 10; i++);
+            {
+                System.out.println("the new number i is " + i);
+            }
+            if (i == 0) {
+                System.out.println("i is equal to zero");
+
+            } else {
+                System.out.println("i is not equal to zero");
+
+            }
+            //what other errors can I put into a source code file?
+        }
     }
-
-    For (Int i=0:i>10;i++){
-        System.out.println("the new number i is "+i);
-    }
-    if (i==0){
-        System.out.print("i is equal to zero");
-
-    }else {
-        system.out.print("i is not equal to zero");
-
-    }
-    //what other errors can I put into a source code file?
-
 }
